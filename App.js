4b1ede67a1bb68cd58fef8488e0f752b0ad8b62e
Ebrar <16155052@mersin.edu.tr>
2023-05-17 14:28:21 +0300

    packages

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.Text}>Welcome the travel Ai Api</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#1abc9c',
+    color: '#ecf0f1',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  Text: {
+    color: '#ecf0f1',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+
 });
